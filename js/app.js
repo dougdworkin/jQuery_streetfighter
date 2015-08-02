7a9bb1fa634c7c52b5369159282583be63b24030
dougdworkin <doug@dougdworkin.com>
2015-08-02 09:40:01 -0700

    added animation and sound and updated js

--- js/app.js
+++ js/app.js
@@ -14,19 +14,37 @@ $(document).ready(function(){
 
 // RYU goes into stance and fires hadouken w/sound on downclick
 	.mousedown(function() {
-		console.log('mousedown event triggered');
 		//play hadouken sound
+		playHadouken();
+		//show and hide positions
 		$('.ryuReady').hide();
 		$('.ryuThrowing').show();
-		$('.hadouken').show();
-		//animate hadouken across screen
+		//show and animate hadouken across the screen
+		$('.hadouken').finish().show().animate (
+			{'left':'1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '529px');
+			}
+		);
 	})
 
 // RYU goes back to ready position on upclick
 	.mouseup(function() {
-		console.log('mouseup event triggered');
 		$('.ryuThrowing').hide();
 		$('.ryuReady').show();
 	});
 
 });
+
+//function to set valume and play hadouken sound
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+
+
+}
+
+
